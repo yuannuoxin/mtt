79bd731a1efe23a8f17ef353a39f7dd39c9477fe
袁林 <3339694043@qq.com>
2026-03-14 03:54:10 +0800

    feat(mybatis): 支持多数据库类型的分页插件配置
    - 移除 mysql-connector-j 的 optional 配置
    - 添加根据驱动类名动态识别数据库类型的逻辑
    - 支持 PostgreSQL、MySQL、Oracle 和 SQL Server 数据库
    - 修改 Docker 构建流程以支持自定义标签
    - 更新 GitHub Actions 工作流配置

--- mtd-common/mtd-common-mybatis/src/main/java/com/mtd/common/mybatis/config/MybatisPlusConfig.java
+++ mtd-common/mtd-common-mybatis/src/main/java/com/mtd/common/mybatis/config/MybatisPlusConfig.java
@@ -3,6 +3,7 @@ package com.mtd.common.mybatis.config;
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,13 +13,35 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class MybatisPlusConfig {
 
+    @Value("${spring.datasource.driver-class-name}")
+    private String driverClassName;
+
     /**
      * 分页插件配置
      */
     @Bean
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
-        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        // 根据驱动类型动态选择数据库类型
+        DbType dbType = getDbType(driverClassName);
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(dbType));
         return interceptor;
+    }
+
+    /**
+     * 根据驱动类名判断数据库类型
+     */
+    private DbType getDbType(String driverClassName) {
+        if (driverClassName.contains("postgresql")) {
+            return DbType.POSTGRE_SQL;
+        } else if (driverClassName.contains("mysql")) {
+            return DbType.MYSQL;
+        } else if (driverClassName.contains("oracle")) {
+            return DbType.ORACLE;
+        } else if (driverClassName.contains("sqlserver")) {
+            return DbType.SQL_SERVER;
+        }
+        // 默认返回 MYSQL
+        return DbType.MYSQL;
     }
 }
